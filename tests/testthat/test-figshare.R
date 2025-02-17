9706f200b05075e39f4380e4844f2ce08528b03f
Adi G <adig@jhu.edu>
2025-02-17 12:34:27 -0500

    Check status of redirected AWS file rather than figshare which is now returning 403s

--- tests/testthat/test-figshare.R
+++ tests/testthat/test-figshare.R
@@ -7,7 +7,7 @@ df = env$mni_figshare
 testthat::test_that("httr::HEAD returns 200 for Figshare", {
 
   results = sapply(df$download_url, function(url) {
-    res = httr::HEAD(url)
+    res = httr::HEAD(strsplit(httr::HEAD(url)$url, split="[?]")[[1]][1])
     testthat::expect_lte(httr::status_code(res), 400)
   })
   testthat::expect_true(all(results == 200))
