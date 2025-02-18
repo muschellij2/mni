949b78757281d83c347a8254a2fda50a5eb40aaa
muschellij2 <muschellij2@gmail.com>
2025-02-18 16:43:30 -0500

    removed bad vignette

--- tests/testthat/test-figshare.R
+++ tests/testthat/test-figshare.R
@@ -4,10 +4,10 @@ data("mni_figshare", envir = env, package = "mni")
 
 df = env$mni_figshare
 
-testthat::test_that("httr::HEAD returns 200 for Figshare", {
+testthat::test_that("httr::GET returns 200 for Figshare", {
 
   results = sapply(df$download_url, function(url) {
-    res = httr::HEAD(url)
+    res = httr::GET(url)
     testthat::expect_lte(httr::status_code(res), 400)
   })
   testthat::expect_true(all(results == 200))
